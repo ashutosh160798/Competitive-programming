555c7cb3163e3b95483f465cd24efefbaab539af
ashutosh <ashutoshaggarwal98@gmail.com>
2019-07-10 05:57:30 +0530

    DP

---
+++ DP/D21_palindrome_substring.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class D21_palindrome_substring {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println(count(s.next()));
+    }
+
+    private static int count(String str) {
+
+        boolean[][] dp = new boolean[str.length()][str.length()];
+        int ans = 0;
+        for (int i = 0; i < dp.length; i++) {
+            dp[i][i] = true;
+            ans++;
+        }
+        for (int i = 1; i < dp.length; i++) {
+            if (str.charAt(i) == str.charAt(i - 1)) {
+                dp[i-1][i] = true;
+                ans++;
+            }
+        }
+        for (int gap = 2; gap < dp.length; gap++) {
+            for (int i = 0, j = i + gap; j < dp[0].length; i++, j++) {
+                if (str.charAt(i) == str.charAt(j) && dp[i + 1][j - 1]) {
+                    dp[i][j] = true;
+                    ans++;
+                }
+            }
+        }
+        return ans;
+
+    }
+
+}
---
+++ DP/D22_gold_mine_tab.java
@@ -0,0 +1,43 @@
+import javax.swing.text.MutableAttributeSet;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class D22_gold_mine_tab {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int r = s.nextInt();
+        int c = s.nextInt();
+        int[][] mat = new int[r][c];
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[0].length; j++) {
+                mat[i][j] = s.nextInt();
+            }
+        }
+        System.out.println(profit(mat));
+    }
+
+    private static int profit(int[][] mat) {
+        int[][] dp = Arrays.copyOf(mat, mat.length);
+        for (int i = mat[0].length - 2; i >= 0; i--) {
+            for (int j = 0; j < mat.length; j++) {
+                if (j == 0) {
+                    dp[j][i] = Math.max(dp[j][i + 1], dp[j + 1][i + 1]) + mat[j][i];
+                    continue;
+                }
+                if (j == mat.length - 1) {
+                    dp[j][i] = Math.max(dp[j][i + 1], dp[j - 1][i + 1]) + mat[j][i];
+                    continue;
+                }
+                dp[j][i] = Math.max(Math.max(dp[j][i + 1], dp[j - 1][i + 1]), dp[j + 1][i + 1]) + mat[j][i];
+            }
+        }
+        int ans = 0;
+        for (int i = 0; i < mat.length; i++) {
+            ans = Math.max(ans, mat[i][0]);
+        }
+        return ans;
+    }
+
+
+}
